b6273d197ba02b8c6cbe736d859978c7b02bc939
Yudi <abd.rahmat.wahyudi@gmail.com>
2025-01-20 10:28:28 +0700

    upload

---
+++ plugins/elementor-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/elementor-pro',
+        'pretty_version' => '3.26.x-dev',
+        'version' => '3.26.9999999.9999999-dev',
+        'reference' => '36d9612fe833b1e3a3c32ee8d22054693be4c18b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/elementor-pro' => array(
+            'pretty_version' => '3.26.x-dev',
+            'version' => '3.26.9999999.9999999-dev',
+            'reference' => '36d9612fe833b1e3a3c32ee8d22054693be4c18b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ plugins/imagify/config/providers.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'Imagify\User\ServiceProvider',
+	'Imagify\Admin\ServiceProvider',
+	'Imagify\Avif\ServiceProvider',
+	'Imagify\CDN\ServiceProvider',
+	'Imagify\Picture\ServiceProvider',
+	'Imagify\Stats\ServiceProvider',
+	'Imagify\Webp\ServiceProvider',
+];
